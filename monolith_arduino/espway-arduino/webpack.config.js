0ece7874a567c32a85d19c179a7952cff404f0aa
dombeef <dombeef@users.noreply.github.com>
2017-08-06 11:39:25 -0400

    initial push
    doesnt work yet, mostly final on lower body housing and just need to add space in upper one

---
+++ monolith_arduino/espway-arduino/webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, 'html-src'),
+
+    entry: {
+        index: './index.js',
+        pid: './pid.js',
+        cube: './cube.js'
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'espway-arduino/data')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tag$/,
+                enforce: 'pre',
+                use: [
+                    { loader: 'babel-loader', options: { presets: ['es2015'] } },
+                    { loader: './riot-loader.js' }
+                ]
+            },
+
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: { presets: ['es2015'] }
+            },
+
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader', 'postcss-loader' ]
+            }
+        ]
+    },
+
+    devServer: {
+        contentBase: path.resolve(__dirname, 'espway-arduino/data')
+    }
+};
+
